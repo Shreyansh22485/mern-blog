09a05add18ca9c6b56472a0dfa6d5df8e71afbd8
Shreyansh22485 <119041890+Shreyansh22485@users.noreply.github.com>
2024-02-01 13:59:52 +0530

    Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 // import Contact from './pages/Contact';
 
 
@@ -26,6 +27,7 @@ export default function App() {
         
         
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
