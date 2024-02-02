df13a7a505ee98228cfc357afe752ba9ad2f6e4c
Shreyansh22485 <119041890+Shreyansh22485@users.noreply.github.com>
2024-02-02 11:56:55 +0530

    sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+
 export const signup = async (req, res, next) => {
   console.log(req.body);
   const { username, email, password } = req.body;
@@ -23,7 +26,40 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     // res.status(500).json({ error: error.message });
     next(error);
+  };
+}
+
+ export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (
+    !email ||
+   
+    !password ||
+    !email.trim() ||
+    
+    !password.trim()
+  ) {
+    // return res.status(400).json({ error: "Please fill all fields" });
+    next(errorHandler(400,'All fields are required'));
   }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler( 404, 'User not found') );
+    }
+    const isMatch = bcryptjs.compareSync(password, user.password);
+    if (!isMatch) 
+     return next(errorHandler(400, 'Invalid credentials'));
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pass , ...rest } = user._doc;
+     res.status(200).cookie("access_token", token, { httpOnly: true }).json(rest);
+    // res.status(200).json({ message: 'Welcome back!' , user });
+  } catch (error) {
+  	// console.log(error);
+    // res.status(401).json({ error: error.message });
+    next(error);
+  }
+}
 
   //   try {
   //     const user = await User.create({ username, email, password });
@@ -31,4 +67,4 @@ export const signup = async (req, res, next) => {
   //   } catch (error) {
   //     res.status(500).json({ error: error.message });
   //   }
-};
+
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Signin = () => {
+
+const SignIn = () => {
+  const [formData, setFormData] =  useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading , setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setFormData({...formData, [e.target.id]: e.target.value.trim()});
+  };
+  // console.log(formData)
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(!formData.email || !formData.password){
+      setErrorMessage("Please fill all the fields");
+      return;
+    }
+    // console.log(formData);
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await fetch('/api/auth/signin',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await response.json();
+      if( data.success === false){
+        setErrorMessage(data.message);
+        return;
+      }
+      setLoading(false);
+      if(response.ok){
+        navigate('/');
+      }
+      
+    }
+    catch(err){
+      console.log(err);
+      setErrorMessage(err.message);
+      setLoading(false);
+    }
+  }
   return (
-    <div>Signin</div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          {/* left side */}
+          <Link to="/" className="text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Bhallu's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            nesciunt voluptas quia, eaque ipsa temporibus dol
+          </p>
+        </div>
+        <div className="flex-1">
+          {/* right side */}
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            
+            <div>
+              <Label value="Your Email"></Label>
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
 
-export default Signin
+                className="w-full"
+                
+              ></TextInput>
+            </div>
+            <div>
+              <Label value="Your Password"></Label>
+              <TextInput
+                type="password"
+                placeholder="******"
+                id="password"
+                onChange={handleChange}
+                className="w-full"
+              ></TextInput>
+            </div>
+            <Button gradientDuoTone={"purpleToPink"} pill type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <><Spinner size={'sm'} />
+                  <span className="pl-3">
+                    Loading ...
+                  </span></>
+                  
+                ) : "Sign In"
+              }
+            </Button>
+          </form>
+          <div className=" flex gap-2">
+            <p className="text-sm mt-5">
+              Don't have an account?{" "}
+              <Link to="/sign-up" className="text-blue-500">
+                Sign Up
+              </Link>
+            </p>
+          </div>
+          {
+            errorMessage && 
+            <Alert type="danger" className="mt-5" color='failure'>
+              {errorMessage}
+            </Alert>
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
